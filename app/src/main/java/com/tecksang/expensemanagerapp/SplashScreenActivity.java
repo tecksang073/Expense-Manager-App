55fbaee743e39c53c9d2ec431f1b5cef6c2d0008
Ho Teck Sang <tecksang51@gmail.com>
2021-10-25 16:22:12 +0800

    Make splash screen to expand the entire window

--- app/src/main/java/com/tecksang/expensemanagerapp/SplashScreenActivity.java
+++ app/src/main/java/com/tecksang/expensemanagerapp/SplashScreenActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -22,6 +23,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
         animation = AnimationUtils.loadAnimation(this, R.anim.animation);
